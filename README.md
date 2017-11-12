160c20a0e951b8474b968cbebaccf09c4a400b7a
Simon Lee <silee001@gmail.com>
2017-11-12 22:28:30 +0100

    Hello world proof of concept with docker compose.
    Basic world chleropath with random data

---
+++ README.md
@@ -0,0 +1,12 @@
+# openaid
+Openaid.se Redux
+
+
+
+# Docker
+This uses docker, specifically docker-compose to create the start the various servers.
+
+It uses:
+  * Ngnix
+  * Node
+  * MySQL
---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, HOST)
+
+console.log(`Listening on http://${HOST}:${PORT}`);
